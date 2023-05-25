a13c6296a15520704bac878b8046ce0078bdeca8
omerfiraat <omerfiraatx@gmail.com>
2023-05-25 17:59:04 +0300

    FirstCommit

--- AICase/AICase/ViewController.swift
+++ AICase/AICase/ViewController.swift
@@ -12,6 +12,7 @@ class ChatViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        hideKeyboardOnTap()
         setupUI()
         addInitialMessage()
         scrollToBottom()
